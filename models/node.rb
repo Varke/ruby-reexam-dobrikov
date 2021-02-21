0287b310330d181431d0d196206072d6241ca04f
Designer Molotoba <mr.varke@mail.ru>
2021-02-21 19:32:27 +0300

    Node class created

---
+++ models/node.rb
@@ -0,0 +1,11 @@
+#The class describing what a tree node looks like
+class Node
+
+    def initialize(title, description, children, parent)
+        @title = title
+        @description = description
+        @children = children
+        @parent = parent
+    end
+
+end
